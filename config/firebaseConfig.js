c6ee7d961ba61cab7daef31d5fd31b45d1e6e321
Yash Rajendra Rathod <114213940+yashr6577@users.noreply.github.com>
2025-07-13 00:19:23 +0530

    Update firebaseConfig.js

--- config/firebaseConfig.js
+++ config/firebaseConfig.js
@@ -1,25 +1,3 @@
-// Import the functions you need from the SDKs you need
-// import { initializeApp } from "firebase/app";
-// import { getAnalytics } from "firebase/analytics";
-// // TODO: Add SDKs for Firebase products that you want to use
-// // https://firebase.google.com/docs/web/setup#available-libraries
-
-// // Your web app's Firebase configuration
-// // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-// const firebaseConfig = {
-//   apiKey: "api key",
-//   authDomain: "vayomitra.firebaseapp.com",
-//   projectId: "vayomitra",
-//   storageBucket: "vayomitra.firebasestorage.app",
-//   messagingSenderId: "268139890986",
-//   appId: "app id",
-//   measurementId: " id"
-// };
-
-// // Initialize Firebase
-// const app = initializeApp(firebaseConfig);
-// const analytics = getAnalytics(app);
-
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore, collection, addDoc ,query, where, getDocs, updateDoc, arrayUnion, doc ,documentId} from "firebase/firestore";
@@ -28,22 +6,14 @@ import * as AuthSession from "expo-auth-session";
 
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBdQq5RUXb3XZSlThrr_o7eV2uMC4kRhlw",
+  apiKey: "",
   authDomain: "vayomitra.firebaseapp.com",
   projectId: "vayomitra",
   storageBucket: "vayomitra.firebasestorage.app",
-  messagingSenderId: "268139890986",
-  appId: "1:268139890986:web:5bbc22d0541cab860cb653",
-  measurementId: "G-L2J0C0M23G"
+  messagingSenderId: "",
+  appId: "",
+  measurementId: ""
 };
-
-// const [request, response, promptAsync] = Google.useAuthRequest({
-//   androidClientId: "YOUR_ANDROID_CLIENT_ID",
-//   iosClientId: "YOUR_IOS_CLIENT_ID",
-//   webClientId: "YOUR_WEB_CLIENT_ID",
-//   redirectUri: AuthSession.getRedirectUrl(), // 👈 Ensure correct redirect URI
-// });
-
 
 // 🔥 Initialize Firebase
 const app = initializeApp(firebaseConfig);
